93b7cadcfc7e9c6a65e9cf7cc709f66a2096f3f8
woniyoon <gellifisch@gmail.com>
2021-08-17 21:59:13 +0900

    포트폴리오 페이지 레전드 스타일 수정

--- src/components/charts/StackedBarChart.js
+++ src/components/charts/StackedBarChart.js
@@ -1,6 +1,7 @@
 // import "./styles.css";
 import React, {useState} from 'react'; 
 import { Line } from "react-chartjs-2";
+import { Row, Col } from 'react-bootstrap'; 
 
 
 function StackedBarChart(props) { 
@@ -32,20 +33,29 @@ function StackedBarChart(props) {
   // };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '0 5px', fontSize: '0.8em' }}>
-      {/* <h4>바 차트 넣을거야</h4> */}
-      { data.map(each => {
-        return (
-          <div style={{ flex: each.ratio, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-            <div style={{ height: '15px', backgroundColor: each.color }}> </div>
-            <div style={{ marginTop: '10px', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-              <div style={{ width: '0.7em', height: '0.7em', backgroundColor: each.color, marginRight: '5px' }}></div>
+    <>
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '0 5px', fontSize: '0.8em' }}>
+        { data.map(each => {
+          return (
+            <div style={{ flex: each.ratio, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+              <div style={{ height: '15px', backgroundColor: each.color }}> </div>
+            </div>
+            );
+        }) }
+      </div>
+      <div style={{ width: '100%', margin: '20px auto 10px', display: 'flex', flexDirection: 'row' }}>
+      {
+        data.map(each => {
+          return (
+            <div style={{ flex: 1 }}>
+              <div style={{ display: 'inline-block', width: '0.7em', height: '0.7em', backgroundColor: each.color, marginRight: '10px' }}></div>
               <span style={{ color: '#828282', marginRight: '5px' }}>{ each.label }</span><span style={{ color: '#4F4F4F', fontWeight: '600' }}>{`${each.value} %` }</span>
             </div>
-          </div>
           );
-      }) }
-    </div>
+        })
+      }
+      </div>
+    </>
   );
 }
 
