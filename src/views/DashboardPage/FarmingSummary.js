1dced98e211cbbd6c81a4250249690c2afa07f0d
woniyoon <gellifisch@gmail.com>
2021-08-19 22:36:06 +0900

    파밍 도움말 모달창 수정

--- src/views/DashboardPage/FarmingSummary.js
+++ src/views/DashboardPage/FarmingSummary.js
@@ -25,27 +25,49 @@ function StakingSummary(props) {
   const [BSCBalance, setBSCBalance] = useState({});
   const dispatch = useDispatch();
 
-  function AlertInfo() {
+  const helpMsgs = [
+    { type: "총 매수 금액", description: "'매수 금액'은 풀에 처음 유동성 공급을 했을 당시 예치한 토큰의 원화 환산 금액입니다." },
+    { type: "총 평가 금액", description: "'평가 금액'은 예치한 토큰의 현재 원화 환산 가치입니다." },
+    { type: "리워드 합계", description: "'리워드'는 현재 수확할 수 있는 리워드를 말합니다. 쌓인 리워드를 수확할 경우 숫자가 초기화 되어 0부터 재시작 합니다." },
+    { type: "누적된 리워드", description: "'누적된 리워드' 는 풀에 처음 유동성 공급을 한 이후부터 현재까지 이미 수확이 끝난 리워드와 현재의 리워드 합계를 합한 총량의 총량입니다. \n *블록 추적 상황에 따라 부정확할 수 있습니다. 참고 용도로만 사용해주세요." },
+    { type: "예상 APR 평균", description: "'예상 APR'은 해당 프로토콜에서 풀에 표기한 연간 이율입니다." },
+    { type: "현재 수익률 평균", description: "'현재 수익률'은 해당 풀에 처음 예치한 이후 발생한 이익을 백분율로 표시한 수치입니다. 예치한 토큰의 가치 변동에 따라 수시로 변화합니다." },
+    { type: "현재 APR 평균", description: "'현재 APR'은 현재 수익률을 365일로 환산한 수치입니다. 예치한 토큰의 가치 변동에 따라 수시로 변화합니다." },
+  ];
+
+  function AlertInfo(nTh) {
+    console.log(nTh.index);
     const [show, setShow] = useState(false);
     if (show) {
       return (
-        <Alert
-          onClose={() => setShow(false)}
-          dismissible
-          style={{
-            backgroundColor: "#F6F6F6",
-            position: "absolute",
-            left: "50%",
-            transform: "translate(-50%)",
-            width: "300px",
-          }}
-        >
-          <Alert.Heading style={{ fontSize: "14px" }}>도움말</Alert.Heading>
-          <p style={{ fontSize: "12px" }}>
-            ‘매수 금액’은 풀에 처음 유동성 공급을 했을 당시 예치한 토큰의 원화
-            환산 금액입니다.
-          </p>
-        </Alert>
+        <>
+          <Alert
+            onClose={() => setShow(false)}
+            dismissible
+            style={{
+              backgroundColor: "#F6F6F6",
+              position: "absolute",
+              left: "50%",
+              transform: "translate(-50%)",
+              width: "300px",
+              zIndex: "2"
+            }}
+          >
+            <Alert.Heading style={{ fontSize: "14px" }}>도움말</Alert.Heading>
+            <p style={{ fontSize: "12px", whiteSpace: "pre-wrap", textAlign: "start" }}>
+              { helpMsgs[nTh.index].description }
+            </p>
+          </Alert>
+          <div style={{
+            position: "absolute", 
+            top: "0", 
+            left: "0", 
+            width: "100vw", 
+            height: "100vh", 
+            backgroundColor: "rgb(0 0 0 / 39%)", 
+            zIndex: "1" 
+          }}> </div>
+        </>
       );
     }
     return (
@@ -140,30 +162,30 @@ function StakingSummary(props) {
       <div className="container-border">
         <p style={{ fontSize: "0.8rem" }}>종합 요약</p>
         <div className="farming-grid">
-          <p>총 매수 금액</p> <AlertInfo />
+          <p>총 매수 금액</p> <AlertInfo index={0} />
           <p style={{ flex: "1", textAlign: "right" }}> ₩3,600,000</p>
         </div>
         <div className="farming-grid">
-          <p>총 평가 금액</p> <AlertInfo />
+          <p>총 평가 금액</p> <AlertInfo index={1} />
           <p style={{ flex: "1", textAlign: "right" }}> ₩4,000,000</p>
         </div>
         <div className="farming-grid">
-          <p>리워드 합계</p> <AlertInfo />
+          <p>리워드 합계</p> <AlertInfo index={2} />
           <p style={{ flex: "1", textAlign: "right" }}> ₩400,000</p>
         </div>
         <div className="farming-grid">
-          <p>수확된 리워드</p> <AlertInfo />
+          <p>수확된 리워드</p> <AlertInfo index={3} />
           <p style={{ flex: "1", textAlign: "right" }}> ₩600,000</p>
-    </div>
-    <div className="farming-grid">
-      <p>예상 APR 평균</p> <AlertInfo />
-      <p style={{ flex: "1", textAlign: "right" }}> 11.11%</p>
-    </div>
-    <div className="farming-grid">
-      <p>현재 수익륜 평균</p> <AlertInfo />
-      <p style={{ flex: "1", textAlign: "right" }}> 11.11%</p>
-    </div>
-  </div>
+        </div>
+        <div className="farming-grid">
+          <p>예상 APR 평균</p> <AlertInfo index={4} />
+          <p style={{ flex: "1", textAlign: "right" }}> 11.11%</p>
+        </div>
+        <div className="farming-grid">
+          <p>현재 수익륜 평균</p> <AlertInfo index={5} />
+          <p style={{ flex: "1", textAlign: "right" }}> 11.11%</p>
+        </div>
+      </div>
 
   <div className="container-border">
     <p style={{ fontSize: "0.8rem" }}>프로토콜별 요약</p>
